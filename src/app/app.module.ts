30b0cf433e311b16ff78ec7949ad5d380776e0d9
sohairabdelhameed <sohairahmaid@gmail.com>
2023-11-07 08:12:34 +0200

    Complete TicTacToa game angular11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SqaureComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule,NbButtonModule} from '@nebular/theme';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SqaureComponent,
+    BoardComponent,
+   
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbButtonModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+  
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,68 @@
+import { Component,OnInit } from "@angular/core";
+@Component({
+    selector:'app-borad',
+   templateUrl:'./borad.component.html',
+   styleUrls:['./board.component.scss']
+
+})
+
+   export class BoardComponent implements OnInit{
+    squares: any[] = [];
+    xIsNext: boolean = false;
+    winner: string | null = null;
+    draw: boolean = false;
+
+
+    constructor(){}
+    ngOnInit() {
+        this.newGame();
+        
+        
+    }
+    newGame(){
+        this.squares = Array(9).fill(null);
+        this.winner= null;
+        this.xIsNext=true;
+        this.draw = false;
+    }
+    get player(){
+        return this.xIsNext ? 'X' : 'O';
+    }
+    makeMove(idx: number) {
+        if (!this.squares[idx] && !this.winner) {
+          this.squares.splice(idx, 1, this.player);
+          this.xIsNext = !this.xIsNext;
+          this.winner = this.calculateWinner();
+          
+          // Check for a draw if there is no winner
+          if (!this.winner && this.squares.every((square) => square)) {
+            this.draw = true;
+          }
+        }
+      }
+      
+    
+        calculateWinner() {
+            const lines = [
+              [0, 1, 2],
+              [3, 4, 5],
+              [6, 7, 8],
+              [0, 3, 6],
+              [1, 4, 7],
+              [2, 5, 8],
+              [0, 4, 8],
+              [2, 4, 6]
+            ];
+            for (let i = 0; i < lines.length; i++) {
+              const [a, b, c] = lines[i];
+              if (
+                this.squares[a] &&
+                this.squares[a] === this.squares[b] &&
+                this.squares[a] === this.squares[c]
+              ) {
+                return this.squares[a];
+              }
+            }
+            return null;
+          }
+        }
